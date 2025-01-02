a4cd53111efae951a029171cc54070ed030c2272
Giga Katamadze <gigakatamadze0@gmail.com>
2025-01-02 12:30:18 +0400

    initial commit for spring backend

---
+++ backend/src/test/java/com/gigaacademy/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigaacademy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
